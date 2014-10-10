c356506b913cda2049e4fd0c631e3b0874cc03f2
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-10-10 16:30:56 -0700

    no hyphens in app name

--- config/initializers/secure_random.rb
+++ config/initializers/secure_random.rb
@@ -1,2 +1,2 @@
 secret = Rails.env.production? ? ENV['SECRET_TOKE'] : "top_secret_token"
-zombie-floss::Application.config.secret_key_base = secret
+ZombieFloss::Application.config.secret_key_base = secret
