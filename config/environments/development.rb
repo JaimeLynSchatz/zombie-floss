cde409975c279a7076187b700b5a4c36cea0dbb3
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-10-11 23:18:34 -0700

    set config.serve_static_assets to false

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,7 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  config.serve_static_assets = false
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
